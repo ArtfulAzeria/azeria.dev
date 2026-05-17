d1b0a3bd47e2a6b61910785d085f4e8e83c0e5ca
azeria <azeriaketturenard@gmail.com>
2026-05-17 21:35:19 +0200

    landing page base

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,14 +1,39 @@
-var uwu = "uwuwowuwu";
-
+var uwu = ["Desarrollador", "Forense Informático", "Técnico", "Diseñador", "Arquitecto"];
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-
 const yourFunction = async () => {
     while (true) {
-        var item = document.getElementById("main-alts").textContent=uwu;
-        await delay(50);
-        var item = document.getElementById("main-alts").textContent="miau";
-        await delay(50);
+        var item = document.getElementById("main-alts").textContent=uwu[0];
+        document.getElementById("main-alts").style.opacity = 1;
+        await delay(2000);
+        document.getElementById("main-alts").style.opacity = 0;
+        await delay(200);
+        
+        var item = document.getElementById("main-alts").textContent=uwu[1];
+        document.getElementById("main-alts").style.opacity = 1;
+        await delay(2000);
+        document.getElementById("main-alts").style.opacity = 0;
+        await delay(200);
+        
+        var item = document.getElementById("main-alts").textContent=uwu[2];
+        document.getElementById("main-alts").style.opacity = 1;
+        await delay(2000);
+        document.getElementById("main-alts").style.opacity = 0;
+        await delay(200);
+
+        var item = document.getElementById("main-alts").textContent=uwu[3];
+        document.getElementById("main-alts").style.opacity = 1;
+        await delay(2000);
+        document.getElementById("main-alts").style.opacity = 0;
+        await delay(200);
+
+        var item = document.getElementById("main-alts").textContent=uwu[4];
+        document.getElementById("main-alts").style.opacity = 1;
+        await delay(2000);
+        document.getElementById("main-alts").style.opacity = 0;
+        await delay(200);
     }
 }
+
+yourFunction();
