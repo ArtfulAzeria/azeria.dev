21cc1421e6d75b89f7f59c4096d151ab1f01df07
azeria <azeriaketturenard@gmail.com>
2026-05-18 13:55:38 +0200

    Rotmainalts

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,38 +1,54 @@
 var uwu = ["Desarrollador", "Forense Informático", "Técnico", "Diseñador", "Arquitecto"];
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function delay(time) {
+        return new Promise(function(resolve) {
+        setTimeout(resolve, time);
+        });
+    }
+
+const transitionDelay = 1000;
+const viewTime = 2000;
 
 const yourFunction = async () => {
     while (true) {
-        var item = document.getElementById("main-alts").textContent=uwu[0];
+        document.getElementById("main-alts").textContent=uwu[0];
         document.getElementById("main-alts").style.opacity = 1;
-        await delay(2000);
-        document.getElementById("main-alts").style.opacity = 0;
-        await delay(200);
         
-        var item = document.getElementById("main-alts").textContent=uwu[1];
-        document.getElementById("main-alts").style.opacity = 1;
-        await delay(2000);
+        await delay(transitionDelay);
+        document.getElementById("main-alts").style.transform = "perspective(5em) rotateX(5deg) scale(1, 0.5)";
+        await delay(viewTime);
         document.getElementById("main-alts").style.opacity = 0;
-        await delay(200);
+        document.getElementById("main-alts").style.transform = "none";
+        await delay(transitionDelay);
         
-        var item = document.getElementById("main-alts").textContent=uwu[2];
+        document.getElementById("main-alts").textContent=uwu[1];
         document.getElementById("main-alts").style.opacity = 1;
-        await delay(2000);
+        await delay(transitionDelay);
+        await delay(viewTime);
         document.getElementById("main-alts").style.opacity = 0;
-        await delay(200);
+        await delay(transitionDelay);
 
-        var item = document.getElementById("main-alts").textContent=uwu[3];
+        document.getElementById("main-alts").textContent=uwu[2];
         document.getElementById("main-alts").style.opacity = 1;
-        await delay(2000);
+        await delay(transitionDelay);
+        await delay(viewTime);
         document.getElementById("main-alts").style.opacity = 0;
-        await delay(200);
+        await delay(transitionDelay);
 
-        var item = document.getElementById("main-alts").textContent=uwu[4];
+        document.getElementById("main-alts").textContent=uwu[3];
         document.getElementById("main-alts").style.opacity = 1;
-        await delay(2000);
+        await delay(transitionDelay);
+        await delay(viewTime);
         document.getElementById("main-alts").style.opacity = 0;
-        await delay(200);
+        await delay(transitionDelay);
+
+        document.getElementById("main-alts").textContent=uwu[4];
+        document.getElementById("main-alts").style.opacity = 1;
+        await delay(transitionDelay);
+        await delay(viewTime);
+        document.getElementById("main-alts").style.opacity = 0;
+        await delay(transitionDelay);
     }
 }
 
